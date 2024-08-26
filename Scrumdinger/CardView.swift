e43372fcda9dbc8096897688fc43849fe1a21a3f
mobigaurav21 <gaurav.kumar@pentair.com>
2024-08-26 17:24:17 -0400

    first commit

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,19 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Kumar, Gaurav on 8/26/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum:DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
