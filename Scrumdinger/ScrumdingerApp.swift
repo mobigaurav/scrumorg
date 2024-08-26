5bec8cf120d93c8983ed9d099ad5fbf73d103f6e
mobigaurav21 <gaurav.kumar@pentair.com>
2024-08-26 12:55:36 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kumar, Gaurav on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
